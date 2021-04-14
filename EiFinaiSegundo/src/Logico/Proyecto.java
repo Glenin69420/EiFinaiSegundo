890bf3987259af7cbdcefbd57f9f3708ac49151b
Nasu4life <Nasu4life@Nasu4life>
2021-04-14 18:16:28 -0400

    Cambio en proyecto

--- EiFinaiSegundo/src/Logico/Proyecto.java
+++ EiFinaiSegundo/src/Logico/Proyecto.java
@@ -7,14 +7,15 @@ public class Proyecto {
 	private String descripcion;
 	private Trabajadora jefe; //Todavia en duda
 	private String Nombre;
-	private String TipoDeProyecto;
+	private int TipoDeProyecto; //1. Web 2. Movil 3. Desktop 
 	
 	
-	public Proyecto(String Nombre, String descripcion, ArrayList<Trabajadora> Trabajadores) {
+	public Proyecto(String Nombre, String descripcion, ArrayList<Trabajadora> Trabajadores, int TipoDeProyecto) {
 		super();
 		this.empleados = Trabajadores;
 		this.descripcion = descripcion;
 		this.setNombre(Nombre);
+		this.TipoDeProyecto = TipoDeProyecto;
 	}
 
 	public ArrayList<Trabajadora> getEmpleados() {
@@ -47,5 +48,13 @@ public class Proyecto {
 
 	public void setNombre(String nombre) {
 		Nombre = nombre;
+	}
+
+	public int getTipoDeProyecto() {
+		return TipoDeProyecto;
+	}
+
+	public void setTipoDeProyecto(int tipoDeProyecto) {
+		TipoDeProyecto = tipoDeProyecto;
 	}
 }
