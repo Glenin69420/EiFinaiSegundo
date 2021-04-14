7062ce99033758b22cfda13160c0ac8a6430f5ea
Frankely <Frankely@LAPTOP-QR5F02R0>
2021-04-14 19:04:46 -0400

    La de verdad

--- EiFinaiSegundo/src/Visual/ProyectoPrincipal.java
+++ EiFinaiSegundo/src/Visual/ProyectoPrincipal.java
@@ -9,12 +9,21 @@ import java.net.URL;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import Logico.Empresa;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class ProyectoPrincipal extends JFrame {
 
@@ -28,7 +37,7 @@ public class ProyectoPrincipal extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -39,12 +48,30 @@ public class ProyectoPrincipal extends JFrame {
 				}
 			}
 		});
-	}
+	}*/
 
 	/**
 	 * Create the frame.
 	 */
 	public ProyectoPrincipal() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				FileOutputStream empresa2;
+				ObjectOutputStream empresaWrite;
+				try {
+					empresa2 = new  FileOutputStream("empresa.dat");
+					empresaWrite = new ObjectOutputStream(empresa2);
+					empresaWrite.writeObject(Empresa.getInstance());
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		setIconImage(Toolkit.getDefaultToolkit().getImage(ProyectoPrincipal.class.getResource("/Imagenes/empresa.png")));
 		setTitle("Empresa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -66,21 +93,75 @@ public class ProyectoPrincipal extends JFrame {
 		});
 		mnNewMenu.add(mntmNewMenuItem);
 		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Clientes");
+		mntmNewMenuItem_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegistroDeCliente C = new RegistroDeCliente();
+				C.setVisible(true);
+				C.setModal(false);
+	
+				
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_3);
+		
 		JMenu mnNewMenu_1 = new JMenu("Listados");
 		menuBar.add(mnNewMenu_1);
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Trabajadores");
-		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Trabajadores");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ListadoDeTrabajadores L = new ListadoDeTrabajadores();
 				L.setVisible(true);
 				L.setModal(false);
 			}
 		});
-		mnNewMenu_1.add(mntmNewMenuItem_1);
+		mnNewMenu_1.add(mntmNewMenuItem_2);
 		
-		JMenu mnNewMenu_2 = new JMenu("New menu");
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Clientes");
+		mntmNewMenuItem_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListadoDeClientes C = new ListadoDeClientes();
+				C.setVisible(true);
+				C.setModal(false);
+			}
+		});
+		mnNewMenu_1.add(mntmNewMenuItem_4);
+		
+		JMenu mnNewMenu_3 = new JMenu("Creaciones");
+		menuBar.add(mnNewMenu_3);
+		
+		JMenuItem mntmNewMenuItem_6 = new JMenuItem("Proyecto");
+		mntmNewMenuItem_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegistrarProyecto P = new RegistrarProyecto();
+				P.setVisible(true);
+			}
+		});
+		mnNewMenu_3.add(mntmNewMenuItem_6);
+		
+		JMenu mnNewMenu_2 = new JMenu("Prueba");
 		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Login");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Login L = new Login();
+				L.setVisible(true);
+				L.setModal(false);
+			}
+		});
+		mnNewMenu_2.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Consultar");
+		mntmNewMenuItem_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ConsultarImformacion I = new ConsultarImformacion();
+				I.setVisible(true);
+				I.setModal(false);
+			}
+		});
+		mnNewMenu_2.add(mntmNewMenuItem_5);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -93,5 +174,5 @@ public class ProyectoPrincipal extends JFrame {
 		super.setSize(dim.width, dim.height-45);
 		setLocationRelativeTo(null);
 	}
-	//xd
+
 }
